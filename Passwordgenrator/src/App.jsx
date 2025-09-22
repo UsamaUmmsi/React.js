286e0d2aed62793dd2637ba8316279f4823227c1
usamakhan <usamakhan@digitalgravity.ae>
2025-09-22 18:22:25 +0500

    updated

---
+++ Passwordgenrator/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useCallback } from 'react'
+import './App.css'
+
+function App() {
+
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [chartAllowed, setchartAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const passwordGenrator = useCallback(() => {
+    let pass = ""
+    let str =
+     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (chartAllowed) str += "!@#$%^&*()_+`-=[]{};|,.<>/?"
+  }, [length, numberAllowed, chartAllowed, setPassword]) 
+
+
+  return (
+    <>
+    <h1 className='text-6xl font-bold'>Passoword Genrator</h1>
+
+      
+    </>
+  )
+};
+
+export default App
